af7e81550bb3769de6ea84e2fe8c8636d5c9f44f
Koumare <ibrahimkoumare5@gmail.com>
2025-04-14 15:14:31 +0200

    Mise en place de l'architecture

---
+++ src/main/java/fr/upsaclay/easychair/EasyChairApplication.java
@@ -0,0 +1,13 @@
+package fr.upsaclay.easychair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyChairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyChairApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/upsaclay/easychair/EasyChairApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.upsaclay.easychair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyChairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
