27361c35217646d79a27dd1ba3e2ce95f6d0a1bf
Alban <Alban@gmail.com>
2025-05-07 12:18:25 +0200

    On peut accéder à la liste des soumissions d'un utilisateurs pour l'instantn par défaut, il faut qu'on ajoute un bouton create soumission pour accéder au formulaire mais la route fonctionne bien en tout cas

--- src/main/java/fr/upsaclay/easychair/repository/SubmissionRepository.java
+++ src/main/java/fr/upsaclay/easychair/repository/SubmissionRepository.java
@@ -1,12 +1,11 @@
 package fr.upsaclay.easychair.repository;
 
 import fr.upsaclay.easychair.model.Submission;
+import fr.upsaclay.easychair.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-
-
     List<Submission> findByTitleIgnoreCase(String title);
 }
